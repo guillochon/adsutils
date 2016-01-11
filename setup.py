6084b4490a27ddd4c0be24a444885217c80aa0cc
guillochon <guillochon@gmail.com>
2016-01-11 04:29:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('./requirements.txt')
+install_reqs = parse_requirements('./requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
